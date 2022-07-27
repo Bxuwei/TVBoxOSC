1696546bcb130a602d4915ff54d1d200da56c287
Bxuwei <86274787+Bxuwei@users.noreply.github.com>
2022-07-27 12:05:22 +0800

    Update SourceViewModel.java

--- app/src/main/java/com/github/tvbox/osc/viewmodel/SourceViewModel.java
+++ app/src/main/java/com/github/tvbox/osc/viewmodel/SourceViewModel.java
@@ -604,15 +604,15 @@ public class SourceViewModel extends ViewModel {
                             str = new String[]{urlInfo.urls};
                         }
                         List<Movie.Video.UrlBean.UrlInfo.InfoBean> infoBeanList = new ArrayList<>();
-    //                    for (String s : str) {
-     //                       if (s.contains("$")) {
-   //                             String[] ss = s.split("\\$");
-    //                            if (ss.length >= 2) {
-  //                                  infoBeanList.add(new Movie.Video.UrlBean.UrlInfo.InfoBean(ss[0], ss[1]));
- //                               }
-   //                           infoBeanList.add(new Movie.Video.UrlBean.UrlInfo.InfoBean(s.substring(0, s.indexOf("$")), s.substring(s.indexOf("$") + 1)));
-  //                          }
-  //                      }
+                      for (String s : str) {
+                            if (s.contains("$")) {
+                             String[] ss = s.split("\\$");
+                            if (ss.length >= 2) {
+                                 infoBeanList.add(new Movie.Video.UrlBean.UrlInfo.InfoBean(ss[0], ss[1]));
+                              }
+                           infoBeanList.add(new Movie.Video.UrlBean.UrlInfo.InfoBean(s.substring(0, s.indexOf("$")), s.substring(s.indexOf("$") + 1)));
+                         }
+                      }
                         for (String s : str) {
                             String[] ss = s.split("\\$");
                             if (ss.length > 0) {
